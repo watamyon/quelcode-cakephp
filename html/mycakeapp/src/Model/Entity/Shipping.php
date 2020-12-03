9494ba7061dddac50fbabe1a15ef7ce4b88082e4
Satu <kttth0320@gmail.com>
2020-12-03 14:38:55 +0900

    インデント修正

--- html/mycakeapp/src/Model/Entity/Shipping.php
+++ html/mycakeapp/src/Model/Entity/Shipping.php
@@ -18,22 +18,22 @@ use Cake\ORM\Entity;
  */
 class Shipping extends Entity
 {
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        'item_id' => true,
-        'bidder_name' => true,
-        'address' => true,
-        'phone_number' => true,
-        'is_shipped' => true,
+	/**
+	 * Fields that can be mass assigned using newEntity() or patchEntity().
+	 *
+	 * Note that when '*' is set to true, this allows all unspecified fields to
+	 * be mass assigned. For security purposes, it is advised to set '*' to false
+	 * (or remove it), and explicitly make individual fields accessible as needed.
+	 *
+	 * @var array
+	 */
+	protected $_accessible = [
+		'item_id' => true,
+		'bidder_name' => true,
+		'address' => true,
+		'phone_number' => true,
+		'is_shipped' => true,
 		'is_received' => true,
-        'biditems' => true,
-    ];
+		'biditems' => true,
+	];
 }
